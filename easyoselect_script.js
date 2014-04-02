0c9515357712a2d2b923f820f11c5375c6fc9561
Carl LeBlond <carl.leblond@iup.edu>
2014-04-01 22:06:04 -0400

    removed default tool

--- easyoselect_script.js
+++ easyoselect_script.js
@@ -1,5 +1,3 @@
-
-
 M.qtype_easyoselect={
     insert_structure_into_applet : function(){
 		var textfieldid = 'id_answer_0';
@@ -50,58 +48,12 @@ M.qtype_easyoselect={
         param.value='true';
 	newApplet.appendChild(param);
 
-	var param=document.createElement('param');
-	param.name='defaultTool';
-        param.value='electronFlow2';
-	newApplet.appendChild(param);
-
         warningspan.appendChild(newApplet);
 
     }
 
 }
 
-/*
-function getSmiles(textfieldid) {
-	document.getElementById(textfieldid).value = document.getElementById('EASYOSELECT' + textfieldid).smiles();
-}
-*/
-
-/*
-function getSmilesEdit(buttonname){
-    var buttonnumber= buttonname.slice(7,-1);
-	textfieldid = 'id_answer_' + buttonnumber;
-	document.getElementById(textfieldid).value = document.getElementById('JME').smiles();
-}
-*/
-
-/*
-///modified by crl for easyoselect sketch
-function getSmilesEdit(buttonname, format){
-    var buttonnumber = buttonname.slice(7,-1);
-    var s = document.MSketch.getMol(format);
-//	s = unix2local(s); // Convert "\n" to local line separator
-	textfieldid = 'id_answer_' + buttonnumber;
-	document.getElementById(textfieldid).value = s;
-}
-*/
-
-
-
-/*
-M.qtype_easyoselect={
-    insert_structure_into_applet : function(){
-		var textfieldid = 'id_answer_0';
-		if(document.getElementById(textfieldid).value != '') {
-		
-		var s = document.getElementById(textfieldid).value;
-		document.MSketch.setMol(s, 'mrv');
-		}
-
-	}
-}
-
-*/
 
 
 M.qtype_easyoselect.init_getanswerstring = function(Y, moodle_version){
