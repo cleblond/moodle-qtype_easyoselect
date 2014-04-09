890c0d7f098ca3c52f4b2cd38b89b46eaa59f383
Carl LeBlond <carl@easyochem.com>
2014-04-09 11:57:09 -0400

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014040800;
+$plugin->version   = 2014040900;
 $plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_easyoselect';
